44376ec49cd6b8496342ffdc00efad1044c2d8ef
afadlih <correct-144323171+afadlih@users.noreply.github.com>
2024-09-19 15:28:58 +0700

    Praktikum Bagian 4 : Penggunaan Struktur Kontrol Pada PHP soal no 4.2

--- dasarWeb/PRAKTIKUM 4/struktur_kontrol.php
+++ dasarWeb/PRAKTIKUM 4/struktur_kontrol.php
@@ -1,6 +1,6 @@
 <?php
+echo "Struktur Kontrol <br><br>";
 $nilaiNumerik = 92;
-
 if($nilaiNumerik >= 90 && $nilaiNumerik <= 100){
     echo "Nilai huruf A";
 } elseif($nilaiNumerik >= 80 && $nilaiNumerik < 90){
@@ -10,4 +10,18 @@ if($nilaiNumerik >= 90 && $nilaiNumerik <= 100){
 } elseif($nilaiNumerik < 70){
     echo "Nilai huruf D";
 } 
+
+echo "<br><br>";
+$jarakSaatIni = 0;
+$jarakTarget = 500;
+$peningkatanHarian = 30;
+$hari = 0;
+
+while ($jarakSaatIni < $jarakTarget) {
+    $jarakSaatIni += $peningkatanHarian;
+    $hari++;
+}
+echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
+
+
 ?>  

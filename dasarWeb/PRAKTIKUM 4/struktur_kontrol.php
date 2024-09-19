5dc4392e270871f7349cc71dfcbad12aca57464e
afadlih <correct-144323171+afadlih@users.noreply.github.com>
2024-09-19 15:30:32 +0700

    Praktikum Bagian 4 : Penggunaan Struktur Kontrol Pada PHP soal no 4.3

--- dasarWeb/PRAKTIKUM 4/struktur_kontrol.php
+++ dasarWeb/PRAKTIKUM 4/struktur_kontrol.php
@@ -23,5 +23,16 @@ while ($jarakSaatIni < $jarakTarget) {
 }
 echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
 
+echo "<br><br>";
+echo "Perulangan For<br>";
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
 
+for ($i = 1; $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
+}
+
+echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
 ?>  

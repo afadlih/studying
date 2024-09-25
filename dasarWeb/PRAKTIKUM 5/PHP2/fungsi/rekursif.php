0def3814c5b81c5c5bb3f7061592342ae0010ff7
afadlih <correct-144323171+afadlih@users.noreply.github.com>
2024-09-25 15:06:16 +0700

    Praktikum 1 Fungsi Rekursif NIlai PHP 2 soal no 7

--- dasarWeb/PRAKTIKUM 5/PHP2/fungsi/rekursif.php
+++ dasarWeb/PRAKTIKUM 5/PHP2/fungsi/rekursif.php
@@ -1,7 +1,15 @@
 <?php
-function tampilkanHaloDunia() {
-echo "Halo dunia! <br>";
-tampilkanHaloDunia();
+for($i=1; $i<=25; $i++){
+    echo "Perulangan ke-{$i} <br>";
 }
-tampilkanHaloDunia();
+echo "<hr>";
+function tampilkanAngka(int $jumlah, int $indeks = 1)
+{
+    echo "Perulangan ke-{$indeks} <br>";
+    //panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+tampilkanAngka(20);
 ?>

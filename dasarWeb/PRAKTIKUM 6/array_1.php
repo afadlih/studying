7101629211aacdff1a98bd5c7be1205062023602
afadlih <correct-144323171+afadlih@users.noreply.github.com>
2024-10-02 20:12:46 +0700

    P6_Praktikum Bagian 1. Indexed Array Soal no 1

--- dasarWeb/PRAKTIKUM 6/array_1.php
+++ dasarWeb/PRAKTIKUM 6/array_1.php
@@ -4,11 +4,20 @@
 </head>
 <body>
     <h2>Array Terindeks</h2>
-    <?php
-    $Listdosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
-    echo $Listdosen [2] . "<br>";
-    echo $Listdosen [0] . "<br>";
-    echo $Listdosen[1] . "<br>";
-    ?>
-</body> 
+    <table>
+        <tr>
+            <th>Index</th>
+            <th>Nama Dosen</th>
+        </tr>
+        <?php
+        $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+        foreach ($Listdosen as $index => $dosen) {
+            echo "<tr>";
+            echo "<td>" . $index . "</td>";
+            echo "<td>" . $dosen . "</td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</body>
 </html>

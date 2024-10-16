5f14318920c967d4ac1f73e0f74d74fa0e1f8c26
afadlih <correct-144323171+afadlih@users.noreply.github.com>
2024-10-16 18:38:22 +0700

    P7_Praktikum 1. Function Isset Soal 1.2

--- dasarWeb/PRAKTIKUM 7/isset.php
+++ dasarWeb/PRAKTIKUM 7/isset.php
@@ -1,7 +1,17 @@
-<?
+
+}<?php
 $umur;
 if (isset($umur) && $umur >= 18) {
-    echo "Amda Sudah Dewasa";
+    echo "Anda Sudah Dewasa";
 } else {
     echo "Anda belum dewasa atau variabel umur tidak ditemukan";
 }
+echo "<br><br>";
+
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
+}
+?>

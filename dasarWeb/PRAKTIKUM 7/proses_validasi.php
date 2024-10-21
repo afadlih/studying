2572df5bedc19577ed2562871d4da4603b794f2c
afadlih <correct-144323171+afadlih@users.noreply.github.com>
2024-10-21 09:55:21 +0700

    P7_Praktikum 7: Validasi Form_Soal 7.3

--- dasarWeb/PRAKTIKUM 7/proses_validasi.php
+++ dasarWeb/PRAKTIKUM 7/proses_validasi.php
@@ -1,20 +1,24 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Ambil data dari formulir
+    // Ambil data dari request AJAX
     $nama = $_POST['nama'];
     $email = $_POST['email'];
+
     // Inisialisasi array untuk menyimpan pesan kesalahan
     $errors = array();
+
     // Validasi nama
     if (empty($nama)) {
         $errors[] = 'Nama harus diisi.';
     }
+
     // Validasi email
     if (empty($email)) {
         $errors[] = 'Email harus diisi.';
     } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $errors[] = 'Format email tidak valid.';
     }
+
     // Jika ada kesalahan validasi, tampilkan pesan kesalahan
     if (!empty($errors)) {
         foreach ($errors as $error) {
@@ -26,3 +30,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         echo 'Data berhasil dikirim: Nama = ' . $nama . ', Email = ' . $email;
     }
 }
+?>

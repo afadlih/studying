98e6f5e8bb8ec6d6f7d407c7b843022d760d8acd
afadlih <correct-144323171+afadlih@users.noreply.github.com>
2024-10-21 09:42:45 +0700

    P7_Praktikum 5: Penggunaan Regex Pada PHP_Soal 5.6

--- dasarWeb/PRAKTIKUM 7/regex.php
+++ dasarWeb/PRAKTIKUM 7/regex.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $pattern = '/[a-z]/'; // Cocokkan huruf kecil.
 $text = 'This is a Sample Text.';
 if (preg_match($pattern, $text)) {
@@ -6,7 +6,7 @@ if (preg_match($pattern, $text)) {
 } else {
     echo "Tidak ada huruf kecil!";
 }
-echo"<br><br>";
+echo "<br><br>";
 $pattern = '/[0-9]+/'; // Cocokkan satu atau lebih digit.
 $text = 'There are 123 apples.';
 if (preg_match($pattern, $text, $matches)) {
@@ -15,14 +15,14 @@ if (preg_match($pattern, $text, $matches)) {
     echo "Tidak ada yang cocok!";
 }
 
-echo"<br><br>";
+echo "<br><br>";
 $pattern = '/apple/';
 $replacement = 'banana';
 $text = 'I like apple pie.';
 $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text; // Output: I like banana pie.
 
-echo"<br><br>";
+echo "<br><br>";
 $pattern = '/go*d/';
 $text = 'god is good';
 if (preg_match($pattern, $text, $matches)) {
@@ -31,11 +31,19 @@ if (preg_match($pattern, $text, $matches)) {
     echo "Tidak ada yang cocok!";
 }
 
-echo"<br><br>";
+echo "<br><br>";
 $pattern = '/appl?e/';
 $text = 'I like apple pie. I like apple pie.';
 $new_text = preg_replace($pattern, 'banana', $text);
 echo $new_text; // Output: I like banana pie. I like banana pie.
 
+echo "<br><br>";
+$pattern = '/o{1,2}/'; // Mencocokkan karakter 'o' yang muncul 1 hingga 2 kali
+$text = 'good food moon box cool';
 
+if (preg_match_all($pattern, $text, $matches)) {
+    echo "Cocokkan ditemukan: " . implode(', ', $matches[0]);
+} else {
+    echo "Tidak ada yang cocok!";
+}
 ?>
